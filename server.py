3485fb5f67485a9eef6a64dc30601945184eb650
moon-strider <iamthekidyoudoknowwhatimean@gmail.com>
2023-06-03 12:54:14 +0300

    server reworked (intermediate commit)

--- server.py
+++ server.py
@@ -1,30 +1,36 @@
-import json
 import argparse
+import threading
+import requests
+import time
+
 from flask import Flask, request
 from flask import jsonify
 
-from game import execute, prepare_game, interception_data
+from game import execute, prepare_game, interception_data, COMMANDS
+
+REQUEST_WAIT_TIME = 3
 
 app = Flask(__name__)
 idata = None
 
-@app.route('/move', methods = ['POST'])
-def move():
-    data = request.get_data().decode('utf-8')
-    
-    # if 'dir' in data.keys:
-    #     game.execute(data['dir'])
-    # if 'cam' in data.keys:
-    #     game.execute(data['cam'])
-        
-    execute(idata, data)
+def recieve_commands(server, endpoint, port):
+    r = requests.request("POST", f"{server}/{endpoint}")
+    cmd = r.json() if 200 <= r.status_code <= 299 else None
+    if cmd.replace("_", " ") in COMMANDS:
+        execute(idata, cmd)
+    time.sleep(REQUEST_WAIT_TIME)
 
-    return jsonify({"status": f"recieved {data}"})
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--ip')
     parser.add_argument('--port')
+    parser.add_argument('--server_ip')
+    parser.add_argument('--server_port')
+    parser.add_argument('--set_cmd_endpoint')
+    parser.add_argument('--recieve_endpoint')
     args = parser.parse_args()
     idata = interception_data(*prepare_game())
-    app.run(host=args.ip, port=int(args.port), debug=True, use_reloader=False)
+    threading.Thread(app.run(host=args.ip, port=int(args.port), debug=True, use_reloader=False))
+    requests.request("POST", f"{args.server_ip}/{args.set_cmd_endpoint}")
+    recieve_commands(args.server_ip, args.recieve_endpoint, args.server_port)
